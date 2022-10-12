a830d19948caec954c3dc7c6abe6d69a2dd63291
Brian Carlsen <carlsen.bri@gmail.com>
2022-10-13 01:23:39 +0200

    Fixed rangees for keithley SMUs.

--- instruments/iv_lab_instruments/smus/keithley_2602.py
+++ instruments/iv_lab_instruments/smus/keithley_2602.py
@@ -1,5 +1,3 @@
-from typing import Tuple
-
 import numpy as np
 from pymeasure.instruments.keithley import Keithley2600 as Keithley2600Base
 from pymeasure.adapters import Adapter
@@ -8,135 +6,133 @@ from iv_lab_controller.base_classes.smu import SMU, RangeValue
 
 
 class Keithley2600(SMU, Keithley2600Base):
-	def __init__(self, adapter: Adapter, **kwargs):
-		Keithley2600Base.__init__(self, adapter, **kwargs)
+    def __init__(self, adapter: Adapter, **kwargs):
+        Keithley2600Base.__init__(self, adapter, **kwargs)
 
-	@property
-	def current_range(self) -> Tuple[RangeValue, RangeValue]:
-		"""
-		:returns: Min and max current values.
-		"""
-		curr_lim = self.compliance_current 
-		return (-curr_lim, curr_lim)
+    @property
+    def current_range(self) -> RangeValue:
+        """
+        :returns: Min and max current values.
+        """
+        return self.source_current_range
 
-	@property
-	def voltage_range(self) -> Tuple[RangeValue, RangeValue]:
-		"""
-		:returns: Min and max voltage values.
-		"""
-		volt_lim = self.compliance_voltage 
-		return (-volt_lim, volt_lim)
+    @property
+    def voltage_range(self) -> RangeValue:
+        """
+        :returns: Min and max voltage values.
+        """
+        return self.source_voltage_range
 
-	def set_voltage(
-		self,
-		voltage: float
-	):
-		"""
-		Sets the applied voltage.
+    def set_voltage(
+        self,
+        voltage: float
+    ):
+        """
+        Sets the applied voltage.
 
-		:param voltage: Desired voltage.
-		"""
-		self.source_voltage = voltage
+        :param voltage: Desired voltage.
+        """
+        self.source_voltage = voltage
 
-	def set_current(
-		self,
-		current: float
-	):
-		"""
-		Sets the applied current.
+    def set_current(
+        self,
+        current: float
+    ):
+        """
+        Sets the applied current.
 
-		:param current: Desired current.
-		"""
-		self.source_current = current
+        :param current: Desired current.
+        """
+        self.source_current = current
 
-	def enable_output(self):
-		"""
-		Enable output.
-		"""
-		self.enable_source()
+    def enable_output(self):
+        """
+        Enable output.
+        """
+        self.enable_source()
 
-	def disable_output(self):
-		"""
-		Disable output.
-		"""
-		self.disable_source()
+    def disable_output(self):
+        """
+        Disable output.
+        """
+        self.disable_source()
 
-	def measure_voltage(self) -> float:
-		"""
-		Measure the voltage.
+    def measure_voltage(self) -> float:
+        """
+        Measure the voltage.
 
-		:returns: Measured voltage.
-		"""
-		return self.voltage
+        :returns: Measured voltage.
+        """
+        return self.voltage
 
-	def measure_current(self) -> float:
-		"""
-		Measure the current.
+    def measure_current(self) -> float:
+        """
+        Measure the current.
 
-		:returns: Measured current.
-		"""
-		return self.current
+        :returns: Measured current.
+        """
+        return self.current
 
-	def measure_iv_curve(
-		self,
-		start: float,
-		stop: float,
-		step: float,
-		rate: float,
-	) -> np.array:
-		"""
-		Measure an IV curve.
+    def measure_iv_curve(
+        self,
+        start: float,
+        stop: float,
+        step: float,
+        rate: float,
+    ) -> np.array:
+        """
+        Measure an IV curve.
 
-		:param start: Start voltage.
-		:param stop: Stop voltage.
-		:param step: Absolute voltage step size.
-		:param rate: Scan rate.
-		:returns: numpy.array of measured values.  
-		"""
-		raise NotImplementedError()
+        :param start: Start voltage.
+        :param stop: Stop voltage.
+        :param step: Absolute voltage step size.
+        :param rate: Scan rate.
+        :returns: numpy.array of measured values.  
+        """
+        raise NotImplementedError()
 
-	def measure_iv_point_by_point(
-		self,
-		start: float,
-		stop: float,
-		step: float,
-		rate: float
-	) -> np.array:
-		"""
-		Measure an IV curve.
+    def measure_iv_point_by_point(
+        self,
+        start: float,
+        stop: float,
+        step: float,
+        rate: float
+    ) -> np.array:
+        """
+        Measure an IV curve.
 
-		:param start: Start voltage.
-		:param stop: Stop voltage.
-		:param step: Absolute voltage step size.
-		:param rate: Scan rate.
-		:returns: numpy.array of measured values.  
-		"""
-		raise NotImplementedError()
+        :param start: Start voltage.
+        :param stop: Stop voltage.
+        :param step: Absolute voltage step size.
+        :param rate: Scan rate.
+        :returns: numpy.array of measured values.  
+        """
+        raise NotImplementedError()
 
-	def measure_chronopotentiometry(
-		self,
-		current: float,
-		time: float
-	) -> np.array:
-		"""
-		Perform a chronopotentiometry experiment.
+    def measure_chronopotentiometry(
+        self,
+        current: float,
+        time: float
+    ) -> np.array:
+        """
+        Perform a chronopotentiometry experiment.
 
-		:param current: Set point current.
-		:param time: Run time.
-		:returns: numpy.array of measured values.  
-		"""
-		raise NotImplementedError()
+        :param current: Set point current.
+        :param time: Run time.
+        :returns: numpy.array of measured values.  
+        """
+        raise NotImplementedError()
 
-	def measure_chronoampometry(
-		self,
-		voltage: float,
-		time: float
-	) -> np.array:
-		"""
-		Perform a chronoampometry experiment.
+    def measure_chronoampometry(
+        self,
+        voltage: float,
+        time: float
+    ) -> np.array:
+        """
+        Perform a chronoampometry experiment.
 
-		:param voltage: Set point voltage.
-		:param time: Run time.
-		:returns: numpy.array of measured values.  
-		"""
-		raise NotImplementedError()
+        :param voltage: Set point voltage.
+        :param time: Run time.
+        :returns: numpy.array of measured values.  
+        """
+        raise NotImplementedError()
